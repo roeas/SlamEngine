7f1f74d052cf1e8fa998491d50832920c944e698
Hinageshi <2247001665@qq.com>
2025-03-15 05:33:20 +0800

    Add third party bat

--- Script/utils.lua
+++ Script/utils.lua
@@ -5,14 +5,14 @@ project("Build")
 
     -- Project, binary and intermediate file paths
     location(path.join(RootPath, "Project"))
-    targetdir(path.join(BinaryPath, "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"))
-    objdir(path.join(IntermediatePath, "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}/%{prj.name}"))
+    targetdir(path.join(BinaryPath, "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}/%{prj.name}"))
+    objdir(path.join(IntermediatePath, "%{cfg.system}-%{cfg.architecture}-%{cfg.buildcfg}/%{prj.name}"))
 
     -- Files
     files
     {
         path.join(RootPath, "Setup.bat"),
-        -- path.join(RootPath, "SetupThirdParty.bat"),
+        path.join(RootPath, "SetupThirdParty.bat"),
         path.join(RootPath, "Script/**.lua"),
     }
 
