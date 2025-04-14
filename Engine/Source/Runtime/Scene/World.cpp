cda9bed2b7821f2ffd8cf2051ddc6c59c0d5a4f3
Hinageshi <2247001665@qq.com>
2025-04-15 01:03:17 +0800

    Add count number when creating entity with duplicate name

--- Engine/Source/Runtime/Scene/World.cpp
+++ Engine/Source/Runtime/Scene/World.cpp
@@ -3,8 +3,56 @@
 namespace sl
 {
 
+namespace
+{
+
+bool EntityNameMatch(std::string_view base, std::string_view str)
+{
+    if (base == str)
+    {
+        return true;
+    }
+
+    size_t baseSize = base.size();
+    size_t strSize = str.size();
+    if (strSize < baseSize + 4 ||
+        str.find(base) != 0 ||
+        str[baseSize] != ' ' ||
+        str[baseSize + 1] != '(' ||
+        str[strSize - 1] != ')')
+    {
+        return false;
+    }
+    for (size_t i = baseSize + 2; i < strSize - 1; ++i)
+    {
+        if (!std::isdigit(str[i]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+}
+
 Entity World::CreateEntity(std::string_view name)
 {
+    uint32_t sameNameCount = 0;
+    std::string newName = name.data();
+    auto view = m_registry.view<TagComponent>();
+    for (auto sameNameEntity : view)
+    {
+        if (EntityNameMatch(name, view.get<TagComponent>(sameNameEntity).m_name))
+        {
+            ++sameNameCount;
+        }
+    }
+    if (sameNameCount > 0)
+    {
+        newName += std::format(" ({})", sameNameCount);
+    }
+
     Entity entity{ m_registry.create() };
     entity.AddComponent<TagComponent>(name.data());
     entity.AddComponent<TransformComponent>();
