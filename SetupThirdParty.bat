357de58677627573997aa84c6379c81ba1d53029
Hinageshi <2247001665@qq.com>
2025-05-19 21:14:44 +0800

    Specify generator and architecture in batch file

--- SetupThirdParty.bat
+++ SetupThirdParty.bat
@@ -17,11 +17,14 @@ if defined MSBUILD_PATH (
 )
 echo.
 
+set "CMAKE_GENERATOR=Visual Studio 17 2022"
+set "CMAKE_ARCHITECTURE=x64"
+
 rem spdlog
 set "SPDLOG_PATH=%THIRD_PARTY_PATH%\spdlog"
 echo [ spdlog ] path: %SPDLOG_PATH%
 cd %SPDLOG_PATH%
-cmake -B build -DSPDLOG_USE_STD_FORMAT=ON -DSPDLOG_NO_EXCEPTIONS=ON
+cmake -G "%CMAKE_GENERATOR%" -A %CMAKE_ARCHITECTURE% -B build -DSPDLOG_USE_STD_FORMAT=ON -DSPDLOG_NO_EXCEPTIONS=ON
 cmake --build build --target spdlog --config Debug
 cmake --build build --target spdlog --config Release
 echo.
@@ -30,7 +33,7 @@ rem SDL
 set "SDL_PATH=%THIRD_PARTY_PATH%\sdl"
 echo [ SDL ] path: %SDL_PATH%
 cd %SDL_PATH%
-cmake -B build
+cmake -G "%CMAKE_GENERATOR%" -A %CMAKE_ARCHITECTURE% -B build
 cmake --build build --target SDL3-shared --config Debug
 cmake --build build --target SDL3-shared --config Release
 echo.
@@ -40,7 +43,7 @@ set "SHADERC_PATH=%THIRD_PARTY_PATH%\shaderc"
 echo [ Shaderc ] path: %SHADERC_PATH%
 cd %SHADERC_PATH%
 python "utils\git-sync-deps"
-cmake -B build -DSHADERC_ENABLE_SHARED_CRT=ON
+cmake -G "%CMAKE_GENERATOR%" -A %CMAKE_ARCHITECTURE% -B build -DSHADERC_ENABLE_SHARED_CRT=ON
 cmake --build build --target shaderc_combined --config Debug
 cmake --build build --target shaderc_combined --config Release
 echo.
@@ -50,7 +53,7 @@ set "ASSIMP_PATH=%THIRD_PARTY_PATH%\assimp"
 echo [ Assimp ] path: %ASSIMP_PATH%
 cd %ASSIMP_PATH%
 
-cmake -B build -DBUILD_SHARED_LIBS=OFF
+cmake -G "%CMAKE_GENERATOR%" -A %CMAKE_ARCHITECTURE% -B build -DBUILD_SHARED_LIBS=OFF
 cmake --build build --target assimp --config Debug
 cmake --build build --target assimp --config Release
 echo.
@@ -59,7 +62,7 @@ rem Tracy
 set "TRACY_PATH=%THIRD_PARTY_PATH%\tracy"
 echo [ Tracy ] path: %TRACY_PATH%
 cd %TRACY_PATH%
-cmake -B build
+cmake -G "%CMAKE_GENERATOR%" -A %CMAKE_ARCHITECTURE% -B build
 cmake --build build --target TracyClient --config Debug
 cmake --build build --target TracyClient --config Release
 echo.
